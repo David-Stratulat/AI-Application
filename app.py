dab7b11d7b388ec2ffc141be8bb1b10af2d2699b
David-Stratulat <dstratulat258@gmail.com>
2026-04-16 18:22:46 +0300

    Separated routes into blueprints

--- app.py
+++ app.py
@@ -1,50 +1,10 @@
-from flask import Flask, jsonify, request, Response
-from service import get_knowledge, get_allKnowledge, add_knowledge
-from exceptions import DuplicateException
+from flask import Flask
+from routes.add_product import add_bp
+from routes.view_products import view_bp
 
 app = Flask(__name__, template_folder="templates")
-
-@app.route('/products', methods=['GET']) 
-def get_products():
-    content = get_allKnowledge() 
-    if not content:
-        return jsonify({"eroare": "Nu exista produse"})
-    return jsonify(content)
-
-@app.route('/product/<int:product_id>', methods=['GET'])
-def get_product(product_id):
-    content = get_knowledge(product_id) #Extragem un produs din baza de date
-    if not content:
-        return jsonify({"eroare": "Produsul nu a fost gasit"})
-    return jsonify(content)
-    
-
-@app.route('/add_product', methods=['POST'])
-def add_product():
-    try:
-        data = request.get_json()
-    except Exception:
-        return jsonify({"eroare": "JSON invalid"}), 400
-
-    try:
-        name = data.get('name')
-        price = data.get('price')
-
-        if not name or not price:
-            return jsonify({"eroare": "Lipsesc date"}), 400
-
-        item = add_knowledge(name, price)
-        return jsonify({"message": "Produs adăugat", "data": item}), 201
-
-    except DuplicateException as e:
-        return jsonify({"eroare": str(e)}), 409
-
-    except Exception as e:
-        return jsonify({"eroare": "Eroare internă"}), 500
-    
-@app.errorhandler(Exception)
-def handle_exception(e):
-     return jsonify({"eroare": "Eroare internă neprevăzută"}), 500
+app.register_blueprint(add_bp)
+app.register_blueprint(view_bp)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
---
+++ routes/add_product.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, jsonify, request
+from service import add_knowledge
+
+add_bp = Blueprint('add_produse', __name__)
+
+@add_bp.route('/add_produs', methods=['POST'])
+def add_produs():
+    data = request.get_json()
+    name = data.get('name')
+    price = data.get('price')
+    if not name or not price:
+        return jsonify({"eroare": "Lipsesc date (nume sau pret)"})
+    try:
+        item = add_knowledge(name, price)
+        return jsonify({
+            "message": "Produsul a fost adăugat",
+            "data": item
+        }), 201
+    except Exception as e: 
+        return jsonify({"eroare": str(e)}),
